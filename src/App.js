b7ca3d7e757ef3ee370a9749a72f7d0dc6195198
muller <honoredi098@gmail.com>
2026-05-12 15:40:05 +0200

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: 'center', marginTop: '50px' }}>
+     <h1>Welcome to my React App!</h1>
+     <p>This is a simple React application.</p>
+     <button onClick={() => alert('Button clicked!')}>Click Me</button>
     </div>
   );
 }
